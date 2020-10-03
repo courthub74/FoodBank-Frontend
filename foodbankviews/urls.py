46a76cb1cb91edb73ca179a631f0b4882e4498bb
Courtney Jacobs <cjacobs124@gmail.com>
2020-10-03 16:38:07 -0400

    Mich List

--- foodbankviews/urls.py
+++ foodbankviews/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('michigan/', views.mich, name="mich"),
 ]
--- foodbankviews/views.py
+++ foodbankviews/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 #HOME
 def home(request):
 	return render(request, "home.html", {})
+
+
+#MICHIGAN
+def mich(request):
+	return render(request, "mich.html", {})
+
