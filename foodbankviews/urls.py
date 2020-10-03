6d3d123c1bb120fc8d6b8d2d54ab7380285057e0
Courtney Jacobs <cjacobs124@gmail.com>
2020-10-03 19:38:39 -0400

    Farmington

--- foodbankviews/urls.py
+++ foodbankviews/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('michigan/', views.mich, name="mich"),
+    path('farmington/', views.farm, name="farm"),
 ]
--- foodbankviews/views.py
+++ foodbankviews/views.py
@@ -9,3 +9,14 @@ def home(request):
 def mich(request):
 	return render(request, "mich.html", {})
 
+#Farmington
+def farm(request):
+	# scrape the Farmington stuff here and then call it through the dictionary
+	import requests
+	import json
+
+	farm = requests.get("https://foodbankapi.herokuapp.com/foodbank/3/?format=json")
+	farmington = json.loads(farm.content)
+
+	return render(request, "farm.html", {'farmington': farmington})
+
