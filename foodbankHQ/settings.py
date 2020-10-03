d16c45c19d0069ac458f833f1bf486f9600d5ea1
Courtney Jacobs <cjacobs124@gmail.com>
2020-10-02 20:48:32 -0400

    Heroku

--- foodbankHQ/settings.py
+++ foodbankHQ/settings.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import django_heroku
+from decouple import config
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -9,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yamuo4v!5g(v2$^q!$1ckvnc%h8)wt+6$w1e4=nb$f8n8y8%g%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -108,3 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, 'static')
+]
+
+
+django_heroku.settings(locals())
